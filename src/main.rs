b5e31b449fe68193545e21b03c9e0370edd4fbf5
Chris Gill <chrisfgill@gmail.com>
2015-01-22 20:47:05 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+
+#[derive(Show,Copy)]
+enum Item {
+    Pawn,
+    King,
+    Queen,
+    Rook,
+    Knight, 
+    Bishop,
+}
+
+type Position = (usize,usize); //change to u8 when rust gets changed!
+fn abs (v: usize) -> usize { //wtf happened to std abs? also change to u8 soon
+    let v = v as i32;
+    if v < 0i32 {
+        (v * -1) as usize
+    } 
+    else {v as usize}
+}
+
+impl Item {
+    fn play_isvalid (&self, from:Position, to:Position, inverted:bool) -> bool {
+        match *self {
+            Item::Pawn => { 
+                if to.0 - from.0 == 1 &&
+                    to.1 - from.1 == 1 {true}
+                else {false}
+            },
+            Item::King => {
+                if abs(to.0 - from.0) < 2 &&
+                    abs(to.1 - from.1) < 2 {true}
+                else {false}
+            },
+            Item::Queen => {true},
+            Item::Rook => {true},
+            Item::Knight => {true},
+            Item::Bishop => {true},
+        }
+    }
+}
+
+#[derive(Show,Copy)]
+enum Player {
+    Black(Item),
+    White(Item),
+}
+
+impl Player {
+
+    /// check play logic for valid moves
+    fn play_isvalid (&self, from: Position , to: Position) -> bool {
+        match *self {
+            Player::Black(item) => item.play_isvalid(from,to,true),
+            Player::White(item) => item.play_isvalid(from,to,false),
+        }
+    }
+}
+
+
+type BoardLayout = [[Option<Player>;8];8];
+
+//#[derive(Show)]
+//struct Board(BoardLayout);
+
+#[derive(Show)]
+struct Game {
+    board: BoardLayout,
+    captured: Vec<Player>,
+}
+
+impl Game {
+    fn new() -> Game  {
+        let mut board = [[None;8];8];
+
+        //setup pawns row
+        board[1] = [Some(Player::White(Item::Pawn));8];
+        board[6] = [Some(Player::Black(Item::Pawn));8];
+
+        //setup home row
+        board[0] = [Some(Player::White(Item::Rook)),
+                    Some(Player::White(Item::Knight)),
+                    Some(Player::White(Item::Bishop)),
+                    Some(Player::White(Item::Queen)),
+                    Some(Player::White(Item::King)),
+                    Some(Player::White(Item::Bishop)),
+                    Some(Player::White(Item::Knight)),
+                    Some(Player::White(Item::Rook))];
+
+        board[7] = [Some(Player::Black(Item::Rook)),
+                    Some(Player::Black(Item::Knight)),
+                    Some(Player::Black(Item::Bishop)),
+                    Some(Player::Black(Item::Queen)),
+                    Some(Player::Black(Item::King)),
+                    Some(Player::Black(Item::Bishop)),
+                    Some(Player::Black(Item::Knight)),
+                    Some(Player::Black(Item::Rook))];
+        Game { board:board, captured:Vec::new() }
+    }
+
+    fn get_player (&self,at:Position) -> &Option<Player> {
+        &self.board[at.0][at.1]
+    }
+
+    fn play(&mut self, from:Position,to:Position)-> bool {
+        println!("{:?}",self.get_player(from));
+        println!("{:?}",self.get_player(to));
+
+        if let &Some(p) = self.get_player(from) { p.play_isvalid(from,to) }
+        else {false}
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+   // println!("{:?}",game);
+    println!("{}",game.play((0,4),(3,3)));
+}
