d1e6a7c020b0403b4451fdc022f032b7bfa3ccfe
viperscape <chrisfgill@gmail.com>
2015-05-28 18:33:10 -0400

    chess moves in ui, update game state

--- examples/chess.rs
+++ examples/chess.rs
@@ -26,16 +26,34 @@ enum Menu {
     Game,
 }
 
+/// game state for game ui
+struct GameState {
+    menu: Menu,
+    select: (Option<chess::Position>,Option<chess::Position>),
+    game: Game,
+    win_dim: (u32,u32),
+}
+
+impl GameState {
+    fn default () -> GameState {
+        GameState {
+            menu: Menu::Main,
+            select: (None,None),
+            game: Game::new(),
+            win_dim: (1024,768),
+        }
+    }
+}
+
 fn main () {
-    let mut game = Game::new();
-    let mut win_dim = (1024,768);
+    let mut gs = GameState::default();
     
     let opengl = OpenGL::_3_2;
     let window = GlutinWindow::new(
         opengl,
         WindowSettings::new(
             "Chess".to_string(),
-            Size { width: win_dim.0, height: win_dim.1 }
+            Size { width: gs.win_dim.0, height: gs.win_dim.1 }
             )
             .exit_on_esc(true)
             .samples(4)
@@ -48,8 +66,6 @@ fn main () {
     let theme = Theme::default();
     let glyph_cache = GlyphCache::new(&font_path).unwrap();
     let mut ui = &mut Ui::new(glyph_cache, theme);
-
-    let mut menu_state = Menu::Main;
     
     for event in event_iter {
         ui.handle_event(&event);
@@ -62,9 +78,15 @@ fn main () {
                 Background::new().rgb(0.2, 0.2, 0.2).draw(ui, gl); //this swaps buffers for us
                 Split::new(PaneId).color(dark_grey()).set(ui);
 
-                build_menu_ui(&mut offset,ui, &mut game, &mut menu_state);
-                build_board_ui(&mut offset,ui, &game, &win_dim, &menu_state);
-                
+                build_menu_ui(&mut offset,ui, &mut gs);
+                build_board_ui(&mut offset,ui, &mut gs);
+
+                if let Some(from) = gs.select.0 {
+                    if let Some(to) = gs.select.1 {
+                        println!("play:{:?}",gs.game.play(from,to));
+                        gs.select = (None,None);
+                    }
+                }
                 
                 // Draw our Ui!
                 ui.draw(c,gl);
@@ -74,29 +96,66 @@ fn main () {
     }
 }
 
-fn build_board_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, game: &Game, win_dim: &(u32,u32), menu_state: &Menu) {
-    match *menu_state {
+fn build_board_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, gs: &mut GameState) {
+    match gs.menu {
         Menu::Game => {
-            let item_dim = (win_dim.0 as f64/8.2, win_dim.1 as f64/10.0);
+            let item_dim = (gs.win_dim.0 as f64/8.2, gs.win_dim.1 as f64/10.0);
 
-            for (i,r) in game.view().iter().enumerate() {
+            for (i,r) in gs.game.view().iter().enumerate() {
                 *offset += 8;
-                for (j,c) in r.iter().enumerate() {
+                for (j,piece) in r.iter().enumerate() {
                     let mut b: Button<_>;
                     if (i == 0) & (j == 0) {
                         b = Button::new().bottom_left_of(PaneId);
                     }
                     else if j == 0 {
-                        let mut id = *offset-8;
+                        let id = *offset-8;
                         b = Button::new().up_from(UiId::Widget(id-j),5.0);
                     }
                     else {
                         b = Button::new().right(5.0);
                     }
 
-                    b.label("X")
+                    // todo: convert to fmt for player pieces
+                    let mut label = "";
+                    if let Some(player) = *piece {
+                        match player {
+                            Player::Black(item) => {
+                                label = match item {
+                                    Item::Pawn => "Pawn",
+                                    Item::Rook(_) => "Rook",
+                                    Item::Knight => "Knight",
+                                    Item::Bishop => "Bishop",
+                                    Item::King(_) => "King",
+                                    Item::Queen => "Queen",
+                                    _ => "",
+                                };
+                            },
+                            Player::White(item) => {
+                                label = match item {
+                                    Item::Pawn => "Pawn",
+                                    Item::Rook(_) => "Rook",
+                                    Item::Knight => "Knight",
+                                    Item::Bishop => "Bishop",
+                                    Item::King(_) => "King",
+                                    Item::Queen => "Queen",
+                                    _ => "",
+                                };
+                            },
+                        }
+                    }
+
+                    //if let Some(ref pos) = gs.select.0 {
+                    //    if *pos == (i as i8,j as i8) { b.color(blue()); }
+                    //}
+                    
+                    b.label(label)
                         .dimensions(item_dim.0, item_dim.1)
                         .react(|| {
+                            if gs.select.0.is_some() {
+                                if !gs.select.1.is_some() { gs.select.1 = Some((i as i8,j as i8)); }
+                            }
+                            else { gs.select.0 = Some((i as i8,j as i8)); }
                         })
                         .set(*offset+j, ui);
                 }
@@ -108,8 +167,8 @@ fn build_board_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, game: &Game, win
     }
 }
 
-fn build_menu_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, game: &mut Game, menu_state: &mut Menu) {
-    match *menu_state {
+fn build_menu_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, gs: &mut GameState) {
+    match gs.menu {
         Menu::Main => {
             *offset +=1;
             Button::new()
@@ -117,8 +176,8 @@ fn build_menu_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, game: &mut Game, 
                 .label("New Game")
                 .dimensions(200.0, 60.0)
                 .react(|| {
-                    *menu_state = Menu::Game;
-                    *game = Game::new();
+                    gs.menu = Menu::Game;
+                    gs.game = Game::new();
                 })
                 .set(*offset, ui);
             
@@ -138,7 +197,7 @@ fn build_menu_ui (offset: &mut usize, ui: &mut Ui<GlyphCache>, game: &mut Game, 
                 .label("Exit Game")
                 .dimensions(200.0, 60.0)
                 .react(|| {
-                    *menu_state = Menu::Main;
+                    gs.menu = Menu::Main;
                 })
                 .set(*offset, ui);
         },
