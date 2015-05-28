d805ecb1dee177624b4d1b01e292a9864c4093fe
viperscape <chrisfgill@gmail.com>
2015-05-28 07:02:29 -0400

    prelim conrod setup

--- examples/chess.rs
+++ examples/chess.rs
@@ -1,3 +1,49 @@
-fn main () {
+extern crate conrod;
+extern crate glutin_window;
+extern crate opengl_graphics;
+extern crate piston;
 
+use conrod::{Background, Colorable, Theme, Ui, Positionable, WidgetId};
+use glutin_window::GlutinWindow;
+use opengl_graphics::{ GlGraphics, OpenGL };
+use opengl_graphics::glyph_cache::GlyphCache;
+use piston::event::*;
+use piston::window::{ WindowSettings, Size };
+use std::path::Path;
+
+fn main () {
+    let opengl = OpenGL::_3_2;
+    let window = GlutinWindow::new(
+        opengl,
+        WindowSettings::new(
+            "Chess".to_string(),
+            Size { width: 1024, height: 768 }
+            )
+            .exit_on_esc(true)
+            .samples(4)
+            );
+
+
+    let event_iter = window.events().ups(180).max_fps(60);
+    let mut gl = GlGraphics::new(opengl);
+    let font_path = Path::new("fonts/SourceCodePro-Regular.otf");
+    let theme = Theme::default();
+    let glyph_cache = GlyphCache::new(&font_path).unwrap();
+    let mut ui = &mut Ui::new(glyph_cache, theme);
+
+    for event in event_iter {
+        ui.handle_event(&event);
+
+        if let Some(args) = event.render_args() {
+            gl.draw(args.viewport(), |c, gl| {
+
+                // Draw the background.
+                Background::new().rgb(0.2, 0.2, 0.2).draw(ui, gl); //this swaps buffers for us
+                
+                // Draw our Ui!
+                ui.draw(c,gl);
+
+            });
+        }
+    }
 }
