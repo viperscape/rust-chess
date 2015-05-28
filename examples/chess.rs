4b716ab29f2f66ea9f141542afd01d01da155191
viperscape <chrisfgill@gmail.com>
2015-05-28 15:08:24 -0400

    fix board ui layout

--- examples/chess.rs
+++ examples/chess.rs
@@ -7,7 +7,7 @@ extern crate chess;
 use chess::{Game,Player,Item,AN};
 
 
-use conrod::{Background, Colorable, Theme, Ui, Positionable, Widget,WidgetId, Button, Labelable,Sizeable};
+use conrod::{Background, Colorable, Theme, Ui, UiId, Positionable, Widget,WidgetId, Button, Labelable,Sizeable};
 use conrod::color::{blue, light_grey, orange, dark_grey, red, white};
 use conrod::{Label, Split, WidgetMatrix, Floating};
 
@@ -20,14 +20,14 @@ use std::path::Path;
 
 fn main () {
     let mut game = Game::new();
-
+    let mut win_dim = (1024,768);
     
     let opengl = OpenGL::_3_2;
     let window = GlutinWindow::new(
         opengl,
         WindowSettings::new(
             "Chess".to_string(),
-            Size { width: 1024, height: 768 }
+            Size { width: win_dim.0, height: win_dim.1 }
             )
             .exit_on_esc(true)
             .samples(4)
@@ -50,7 +50,7 @@ fn main () {
                 // Draw the background.
                 Background::new().rgb(0.2, 0.2, 0.2).draw(ui, gl); //this swaps buffers for us
 
-                build_board_ui(0,ui, &game);
+                build_board_ui(0,ui, &game, &win_dim);
                 
                 // Draw our Ui!
                 ui.draw(c,gl);
@@ -60,25 +60,29 @@ fn main () {
     }
 }
 
-fn build_board_ui (offset: usize, ui: &mut Ui<GlyphCache>, game: &Game) {
-    Split::new(offset+1).color(dark_grey()).set(ui);
+fn build_board_ui (offset: usize, ui: &mut Ui<GlyphCache>, game: &Game, win_dim: &(u32,u32)) {
+    let mut offset = offset+1;
+    Split::new(offset).color(dark_grey()).set(ui);
     
-    Button::new()
-        .bottom_left_of(offset+1)
-        .label("X")
-        .dimensions(40.0, 40.0)
-        .react(|| {
-        })
-        .set(offset+2, ui);
-
-    let offset = offset+2;
+    let item_dim = (win_dim.0 as f64/8.2, win_dim.1 as f64/10.0);
 
     for (i,r) in game.view().iter().enumerate() {
+        offset += 8;
         for (j,c) in r.iter().enumerate() {
-            Button::new()
-                .right(5.0)
-                .label("X")
-                .dimensions(40.0, 40.0)
+            let mut b: Button<_>;
+            if (i == 0) & (j == 0) {
+                b = Button::new().bottom_left_of(offset-8);
+            }
+            else if j == 0 {
+                let mut id = offset-8;
+                b = Button::new().up_from(UiId::Widget(id-j),5.0);
+            }
+            else {
+                b = Button::new().right(5.0);
+            }
+
+            b.label("X")
+                .dimensions(item_dim.0, item_dim.1)
                 .react(|| {
                 })
                 .set(offset+j, ui);
